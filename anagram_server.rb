022830f31118c9c154382a642768cd76d5e554c3
Vish Vishvanath <vish.vishvanath@gmail.com>
2014-07-24 12:18:25 +0100

    Accept N words.
    Merges results into a single hash.

--- anagram_server.rb
+++ anagram_server.rb
@@ -3,16 +3,21 @@ require './anagram'
 require 'pry'
 require 'json'
 
-class AnagramServer < Sinatra::Base  
+class AnagramServer < Sinatra::Base
+  def find_anagram(word)
+    result = Anagram.instance.find_anagrams(word)
+    { word.to_sym => result }.to_json
+  end
+
   get '/' do
     'Welcome!'
   end
-  
+
   get '/:word' do
     content_type :json
-    anagram = Anagram.instance
-    result = anagram.find_anagrams(params[:word])
-    { params[:word].to_sym => result }.to_json
+    input = params[:word].split(',')
+    output = input.map { |i| JSON.parse(find_anagram(i)) }.reduce({}, :merge)
+    output.to_json
   end
-  
+
 end
